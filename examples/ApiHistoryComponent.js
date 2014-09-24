30cdcb2651fb9255403d0ebe5e632e4be83c65df
Clement Levasseur <clevasseur@wanadev.fr>
2014-09-24 09:57:22 +0200

    sample + image picking

---
+++ examples/ApiHistoryComponent.js
@@ -0,0 +1,48 @@
+/**
+ * @module Wanaplan
+ * @submodule Component
+ */
+
+var ApiHistoryComponent = (function() {
+
+    this.CAMERACHANGEDACTION = 0;
+
+    var comp = function(core) {
+        BaseComponent3D.call(this, core, 'ApiHistoryComponent');
+        return this;
+    };
+
+    comp.prototype = new BaseComponent3D();
+
+    comp.prototype.initialize = function() {
+        API.registerAction(this.CAMERACHANGEDACTION, this.undoChange, this.redoChange, this);
+        this.startListening();
+    };
+
+    comp.prototype.startListening = function() {
+        this.onCameraChanged = this.onCameraChanged.bind(this);
+
+        document.addEventListener("wnp.request.cameraChanged", this.onCameraChanged, false);
+    };
+
+    comp.prototype.stopListening = function() {
+        document.removeEventListener("wnp.request.cameraChanged", this.onCameraChanged, false);
+    };
+
+    // Callback annulation du déplacement
+    comp.prototype.undoChange = function(target, params) {
+        ujs.notify("wnp.request.cameraChanged");
+    };
+
+    // Callback remise du déplacement
+    comp.prototype.redoChange = function(target, params) {
+        ujs.notify("wnp.request.cameraChanged");
+    };
+
+    comp.prototype.onCameraChanged = function() {
+        API.addHistory(null, null, this.CAMERACHANGEDACTION, this);
+    }
+
+    return comp;
+
+})();
