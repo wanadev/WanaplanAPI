d6aa005237e8c064265700e7540e7d85876c8c5f
perriern <nola.perrier@gmail.com>
2015-01-13 17:29:31 +0100

    Update ApiEditionComponent.js

--- examples/Edition/ApiEditionComponent.js
+++ examples/Edition/ApiEditionComponent.js
@@ -4,7 +4,14 @@
  */
 
 var ApiEditionComponent = (function() {
-
+    /**
+     * Component allowing to group all objects of a floor on middle click (virtual group)
+     * Also uses an external widget to display a green mark at last position when moving objects
+     *
+     * @class ApiEditionComponent
+     * @constructor
+     * @extends BaseComponent3D
+     */
     var comp = function(core) {
         BaseComponent3D.call(this, core, 'ApiEditionComponent');
 
@@ -16,8 +23,8 @@ var ApiEditionComponent = (function() {
     comp.prototype = new BaseComponent3D();
 
     comp.prototype.initialize = function() {
-        API.e3D.addWidget("path/Move.js","Move");
-        this.startListening();
+        //external widget that leaves trace when moving an object
+        API.e3D.addWidget("path/to/file/Move.js","Move")
     };
 
     comp.prototype.startListening = function() {
@@ -30,18 +37,16 @@ var ApiEditionComponent = (function() {
 
     comp.prototype.onClick = function(event) {
         if (event.button !== 1) return;
+        API.e3D.deselectObject();
 
         var collided = event.collided;
         var objects = API.getObjects();
 
         for (var i = 0; i < objects.length; i++) {
-            API.e3D.addToGroup( objects[i].objectInstance , API.e3D.getSelectedObject() );
-
-            API.e3D.deselectObject();
+            API.e3D.addToGroup( objects[i].objectInstance );
         }
 
-        API.e3D.selectObject(collided.pickedMesh.getTopLevelObject());            
-    }
+    };
 
 
     return comp;
